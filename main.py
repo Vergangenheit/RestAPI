cf3738d385f4385765bdba6741bd7220f45de34e
Vergangenheit <lorenzo.ostano@gmail.com>
2020-07-23 13:49:55 +0200

    54:48

--- main.py
+++ main.py
@@ -1,31 +1,47 @@
 from flask import Flask, request
-from flask_restful import Api, Resource, reqparse, abort
+from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+
+# model to store video
+class VideoModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    views = db.Column(db.Integer, nullable=False)
+    likes = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f"Video(name={name}, views={views}, likes={likes})"
+
+
+#db.create_all()
 
 video_put_args = reqparse.RequestParser()
 video_put_args.add_argument("name", type=str, help="Name of the video", required=True)
 video_put_args.add_argument("views", type=int, help="Viiews of the video", required=True)
 video_put_args.add_argument("likes", type=int, help="Likes of the video", required=True)
 
-videos = {}
-
-
-def abort_if_video_doesnt_exist(video_id):
-    if video_id not in videos:
-        abort(404, message="Video id is not valid...")
-
 
 class Video(Resource):
     def get(self, video_id: int):
-        abort_if_video_doesnt_exist(video_id)
-        return videos[video_id]
+        #we're going to query the database
+        result = VideoModel.query.get(id=video_id)
+        return result
 
     def put(self, video_id):
         args = video_put_args.parse_args()
         videos[video_id] = args
         return videos[video_id], 201
+
+    def delete(self, video_id):
+        abort_if_video_doesnt_exist(video_id)
+        del videos[video_id]
+        return '', 204
 
 
 api.add_resource(Video, "/video/<int:video_id>")
--- test.py
+++ test.py
@@ -2,8 +2,15 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-# response = requests.put(BASE + 'video/1', {'likes':20, 'name':'Lollo', 'views':123})
-# print(response.json())
-# input()
-response = requests.get(BASE + 'video/6')
+data = [{'likes':20, 'name':'Lollo', 'views':123},
+        {'likes':4, 'name':'Pillo', 'views':45},
+        {'likes':204, 'name':'Calo', 'views':235}]
+for i in range(len(data)):
+    response = requests.put(BASE + 'video/' + str(i), data[i])
+    print(response.json())
+
+response = requests.delete(BASE + "video/0")
+print(response)
+input()
+response = requests.get(BASE + 'video/2')
 print(response.json())
