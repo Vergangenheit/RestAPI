c29632b84debf2bc0dafc7c3990adfe22d013dc1
Vergangenheit <lorenzo.ostano@gmail.com>
2020-07-23 07:56:09 +0200

    39:24

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
 from flask import Flask, request
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse, abort
 
 app = Flask(__name__)
 api = Api(app)
 
-names = {"tim": {"age": 10, "height": 180},
-         "bill": {"age": 50, "height": 178}}
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument("name", type=str, help="Name of the video", required=True)
+video_put_args.add_argument("views", type=int, help="Viiews of the video", required=True)
+video_put_args.add_argument("likes", type=int, help="Likes of the video", required=True)
 
 videos = {}
 
 
+def abort_if_video_doesnt_exist(video_id):
+    if video_id not in videos:
+        abort(404, message="Video id is not valid...")
+
+
 class Video(Resource):
     def get(self, video_id: int):
+        abort_if_video_doesnt_exist(video_id)
         return videos[video_id]
 
     def put(self, video_id):
-        return {"data": video_id}
+        args = video_put_args.parse_args()
+        videos[video_id] = args
+        return videos[video_id], 201
 
 
 api.add_resource(Video, "/video/<int:video_id>")
--- test.py
+++ test.py
@@ -2,5 +2,8 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.put(BASE + 'video/', {})
+# response = requests.put(BASE + 'video/1', {'likes':20, 'name':'Lollo', 'views':123})
+# print(response.json())
+# input()
+response = requests.get(BASE + 'video/6')
 print(response.json())
