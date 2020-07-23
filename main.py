ec8513ee2466e281cb4bd9acbd90892007be50c5
Vergangenheit <lorenzo.ostano@gmail.com>
2020-07-23 16:31:07 +0200

    end of tutorial

--- main.py
+++ main.py
@@ -19,27 +19,67 @@ class VideoModel(db.Model):
         return f"Video(name={name}, views={views}, likes={likes})"
 
 
-#db.create_all()
+# db.create_all()
 
 video_put_args = reqparse.RequestParser()
 video_put_args.add_argument("name", type=str, help="Name of the video", required=True)
-video_put_args.add_argument("views", type=int, help="Viiews of the video", required=True)
+video_put_args.add_argument("views", type=int, help="Views of the video", required=True)
 video_put_args.add_argument("likes", type=int, help="Likes of the video", required=True)
+
+video_update_args = reqparse.RequestParser()
+video_update_args.add_argument("name", type=str, help="Name of the video")
+video_update_args.add_argument("views", type=int, help="Views of the video")
+video_update_args.add_argument("likes", type=int, help="Likes of the video")
+
+# we define the object serialization
+resource_fields = {
+    'id': fields.Integer,
+    'name': fields.String,
+    'views': fields.Integer,
+    'likes': fields.Integer
+}
 
 
 class Video(Resource):
+    @marshal_with(
+        resource_fields)  # means take the object that we're going to return and serialize it using resource_fields
     def get(self, video_id: int):
-        #we're going to query the database
-        result = VideoModel.query.get(id=video_id)
+        # we're going to query the database
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if not result:
+            abort(404, message="Could not find video with that id")
         return result
 
+    @marshal_with(resource_fields)
     def put(self, video_id):
         args = video_put_args.parse_args()
-        videos[video_id] = args
-        return videos[video_id], 201
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if result:
+            abort(409, message="Video id taken...")
+        # make a new object in the db
+        video = VideoModel(id=video_id, name=args['name'], views=args['views'], likes=args['likes'])
+        db.session.add(video)
+        db.session.commit()
+        return video, 201
+
+    @marshal_with(resource_fields)
+    def patch(self, video_id):
+        args = video_update_args.parse_args()
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if not result:
+            abort(404, message="Video doesn't exist, cannot update...")
+        if args["name"]:
+            result.name = args['name']
+        if args["views"]:
+            result.views = args['views']
+        if args["likes"]:
+            result.likes = args['likes']
+
+        db.session.commit()
+
+        return result
 
     def delete(self, video_id):
-        abort_if_video_doesnt_exist(video_id)
         del videos[video_id]
         return '', 204
 
--- test.py
+++ test.py
@@ -2,15 +2,14 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-data = [{'likes':20, 'name':'Lollo', 'views':123},
-        {'likes':4, 'name':'Pillo', 'views':45},
-        {'likes':204, 'name':'Calo', 'views':235}]
-for i in range(len(data)):
-    response = requests.put(BASE + 'video/' + str(i), data[i])
-    print(response.json())
+# data = [{'likes':20, 'name':'Lollo', 'views':123},
+#         {'likes':4, 'name':'Pillo', 'views':45},
+#         {'likes':204, 'name':'Calo', 'views':235}]
+# for i in range(len(data)):
+#     response = requests.put(BASE + 'video/' + str(i), data[i])
+#     print(response.json())
 
-response = requests.delete(BASE + "video/0")
-print(response)
-input()
-response = requests.get(BASE + 'video/2')
+
+# input()
+response = requests.patch(BASE + 'video/2', {"views":99, 'likes':1001})
 print(response.json())
